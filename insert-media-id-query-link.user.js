4cb5ed20b760fbaab45f02c3fde82c273dd85815
mechchorogi <mechchorogi@gmail.com>
2023-09-28 12:05:35 +0900

    refactor: Update meta @version

--- insert-media-id-query-link.user.js
+++ insert-media-id-query-link.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Insert AV ID link
-// @version      0.3
+// @version      0.4
 // @description  hehe
 // @author       mechchorogi
 // @match        https://adult.contents.fc2.com/article/*/
