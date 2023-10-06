1cd69cccd62274164dbe84846f9db9726d3df35e
mechchorogi <mechchorogi@gmail.com>
2023-10-06 14:37:07 +0900

    feat: Enhanced media ID parsing for dmm.co.jp

--- insert-media-id-query-link.user.js
+++ insert-media-id-query-link.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Insert AV/IV query link
-// @version      0.5
+// @version      0.6
 // @description  Insert AV/IV query link
 // @author       mechchorogi
 // @match        https://adult.contents.fc2.com/article/*/
@@ -32,9 +32,7 @@ const generateIVQueryURL = queryString => `https://watchjavidol.com/?s=${querySt
         targetElement.appendChild(searchLink);
     };
 
-    let mediaId = "";
-    let insertSelector = "";
-    let queryUrl = "";
+    let mediaId, insertSelector, queryUrl;
     switch (document.location.host) {
     case "adult.contents.fc2.com":
         const lastSegment = document.location.href.split('/').slice(-2)[0];
@@ -43,7 +41,16 @@ const generateIVQueryURL = queryString => `https://watchjavidol.com/?s=${querySt
         queryUrl = generateAVQueryURL(mediaId);
         break;
     case "www.dmm.co.jp":
-        mediaId = document.location.href.replace(/.*\/cid=(\D+)(0*)(\d+)\/.*/, "$1-$3");
+        preMediaId = document.location.href.replace(/.*\/cid=[h\d_]*(\D+)(\d+)\/.*/, "$1-$2");
+        mediaId = (() => {
+            const regex = /(\w+)-0*(\d+)/;
+            const match = preMediaId.match(regex);
+            if (!match) { return ""; }
+            const number = parseInt(match[2], 10);
+            const paddedNumber = number.toString().padStart(3, '0');
+            return `${match[1]}-${paddedNumber}`;
+        })();
+        if (mediaId == "") break;
         insertSelector = "div.hreview";
         queryUrl = generateAVQueryURL(mediaId);
         break;
