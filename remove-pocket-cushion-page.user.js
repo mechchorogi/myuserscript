a01d72be4438166a23e41a49d54fecc654ab4278
mechchorogi <mechchorogi@gmail.com>
2025-05-18 23:05:23 +0900

    Add remove-pocket-cushion-page.user.js

---
+++ remove-pocket-cushion-page.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Remove pocket cushion page
+// @namespace    https://getpocket.com
+// @version      0.1
+// @description  Remove cushion page on getpocket.com
+// @author       mechchorogi
+// @match        https://getpocket.com/my-list
+// @match        https://getpocket.com/my-list/*
+// @icon         https://www.google.com/s2/favicons?domain=getpocket.com
+// @grant        none
+// ==/UserScript==
+
+function remove_cushion() {
+    document.querySelectorAll('article.grid').forEach(article => {
+        let article_url = article.querySelector('a.publisher').href;
+        article.querySelectorAll('a').forEach(a => {a.href = a.href != article_url ? article_url : a.href})
+    });
+}
+
+(function() {
+    'use strict';
+    setInterval(remove_cushion, 500);
+})();
