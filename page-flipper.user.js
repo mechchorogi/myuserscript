13f5d571788a2fa3bfeab752b15334ad29d3fb86
mechchorogi <mechchorogi@gmail.com>
2025-05-18 22:52:42 +0900

    Add page-flipper.user.js

---
+++ page-flipper.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Page Flipper
+// @namespace    http://tampermonkey.net/
+// @version      0.2.0
+// @description  Use arrow keys to flip pages on supported sites
+// @match        https://hitomi.la/*
+// @match        https://tktube.com/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    const keyBindingsBySite = {
+        'hitomi.la': {
+            ArrowLeft: 'a[rel=prev]',
+            ArrowRight: 'a[rel=next]'
+        },
+        'tktube.com': {
+            ArrowLeft: 'div.pagination-holder > ul > li.prev a',
+            ArrowRight: 'div.pagination-holder > ul > li.next a'
+        }
+    };
+
+    const currentHost = location.hostname;
+    const keyToSelectorMap = keyBindingsBySite[currentHost];
+    if (!keyToSelectorMap) return;
+
+    function handleKeyDown(event) {
+        const selector = keyToSelectorMap[event.key];
+        if (!selector) return;
+
+        const target = document.querySelector(selector);
+        if (target) {
+            target.click();
+        }
+    }
+
+    window.addEventListener('keydown', handleKeyDown);
+})();
