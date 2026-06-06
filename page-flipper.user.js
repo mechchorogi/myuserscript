011d6ec331999ee2a0e915f93a626fb96c6d272d
mechchorogi <mechchorogi@gmail.com>
2026-06-06 19:34:55 +0900

    [page-flipper.user.js] Update version to 2.1.1

--- page-flipper.user.js
+++ page-flipper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Page Flipper
 // @namespace    http://tampermonkey.net/
-// @version      2.1.0
+// @version      2.1.1
 // @description  Use arrow keys to flip pages via rel=next/prev links on any site
 // @match        http://*/*
 // @match        https://*/*
