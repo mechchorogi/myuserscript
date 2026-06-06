3a671f4c546f20560043cabe67097029240ea417
mechchorogi <mechchorogi@gmail.com>
2026-06-06 20:11:09 +0900

    [hitomi-redirect.user.js] Add Hitomi redirect user script
    This script redirects hitomi.la all/root pages to their Japanese counterparts.
    It handles various page types and ensures users are directed to the correct URLs.

---
+++ hitomi-redirect.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         Hitomi::Redirect
+// @namespace    http://hitomi.la/
+// @version      1.0.0
+// @description  Redirect hitomi.la all/root pages to Japanese pages
+// @author       mechchorogi
+// @match        https://hitomi.la/
+// @match        https://hitomi.la/artist/*-all.html
+// @match        https://hitomi.la/tag/*-all.html
+// @match        https://hitomi.la/series/*-all.html
+// @match        https://hitomi.la/character/*-all.html
+// @match        https://hitomi.la/group/*-all.html
+// @match        https://hitomi.la/type/*-all.html
+// @icon         https://www.google.com/s2/favicons?domain=hitomi.la
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const { pathname } = window.location;
+    let redirectPath = null;
+
+    if (pathname === '/') {
+        redirectPath = '/index-japanese.html';
+    } else {
+        redirectPath = pathname.replace(
+            /^\/(artist|tag|series|character|group|type)\/(.+)-all\.html$/,
+            '/$1/$2-japanese.html'
+        );
+
+        if (redirectPath === pathname) {
+            redirectPath = null;
+        }
+    }
+
+    if (redirectPath) {
+        window.location.replace(new URL(redirectPath, window.location.href).href);
+    }
+})();
