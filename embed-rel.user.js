79ee8461e74f7460a55c3632647879584c4ff1fd
mechchorogi <mechchorogi@gmail.com>
2026-06-06 18:24:40 +0900

    [embed-rel.user.js] Fix a CSS selector for iwara.tv pagination

--- embed-rel.user.js
+++ embed-rel.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Embed rel attributes
 // @namespace    http://tampermonkey.net/
-// @version      1.1.0
+// @version      1.1.1
 // @description  Add rel=prev/next to pager links (e.g. for hitomi.la)
 // @author       mechchorogi
 // @match        http*://*/*
@@ -52,17 +52,15 @@
             let success = false;
             const prevSvg = document.querySelector('.pagination li svg.fa-angle-left');
             if (prevSvg) {
-                const prevLi = prevSvg.closest('li');
-                if (prevLi) {
-                    prevLi.setAttribute('rel', 'prev');
+                if (prevSvg) {
+                    prevSvg.setAttribute('rel', 'prev');
                     success = true;
                 }
             }
             const nextSvg = document.querySelector('.pagination li svg.fa-angle-right');
             if (nextSvg) {
-                const nextLi = nextSvg.closest('li');
-                if (nextLi) {
-                    nextLi.setAttribute('rel', 'next');
+                if (nextSvg) {
+                    nextSvg.setAttribute('rel', 'next');
                     success = true;
                 }
             }
