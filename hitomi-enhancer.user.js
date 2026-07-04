110aff69717c7337b4c2fe250f7ddbbfa2bcf4d2
mechchorogi <mechchorogi@gmail.com>
2026-07-04 10:21:41 +0900

    [hitomi-enhancer.user.js] Update version to 1.1.0

--- hitomi-enhancer.user.js
+++ hitomi-enhancer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hitomi::Enhancer
 // @namespace    http://hitomi.la/
-// @version      1.0.0
+// @version      1.1.0
 // @description  Enhance hitomi.la with small layout and navigation tweaks
 // @author       mechchorogi
 // @match        https://hitomi.la/*
